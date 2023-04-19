bf122b9404e8fc1789cad3d77cddad92c0d36eab
longdeptrai20 <maiduclong11@gmail.com>
2023-04-19 10:34:54 +0700

    first commit

--- crawl_tiktok/tiktok/__init__.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-engine = create_engine('mysql+pymysql://root:mailong2000@localhost:3306/tiktok')
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
